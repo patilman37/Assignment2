dbc9a408d3c9da44ced2f719e7983259adc436ba
patilman37 <72056851+patilman37@users.noreply.github.com>
2020-10-15 17:18:09 +0530

    Add files via upload

---
+++ Hollowinvertedhalf.java
@@ -0,0 +1,26 @@
+class Hollowinvertedhalf
+{
+     public static void main(String args[])
+     {
+       for(int i=1;i<=6;i++)
+       { 
+         System.out.print("* ");
+       }
+      System.out.println();
+     for(int j=5;j>=1;j--)
+     {
+      for(int i=1;i<=j;i++)
+      {
+       if(i == 1 || i == j)
+       {
+         System.out.print("* ");
+       }
+       else
+       {
+        System.out.print(" ");
+       }
+      }
+      System.out.println();
+     }
+    }
+   }
