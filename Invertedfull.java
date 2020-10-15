96ccd449d45237341144e9f3d0e50c8bce40afb8
patilman37 <72056851+patilman37@users.noreply.github.com>
2020-10-15 17:20:43 +0530

    Add files via upload

---
+++ Invertedfull.java
@@ -0,0 +1,18 @@
+class Invertedfull
+{
+    public static void main(String args[])
+    {
+      for(int i=1;i<=6;i++)
+      {
+        for(int j=1;j<=i;j++)
+        {
+          System.out.print(" ");
+        }
+      for(int r=6;r>=i;r--)
+      {
+        System.out.print(" *");
+      }
+      System.out.println();
+     }
+    }
+}
